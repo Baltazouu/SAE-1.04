c023a27a43f75d294ecad2c545b031d2dc523992
Baptiste DUDONNE <badudonne@iutclinfa1912l.iut.local>
2022-12-06 16:56:09 +0100

    ajout creates tables

--- sql-scripts/creation.sql
+++ sql-scripts/creation.sql
@@ -3,19 +3,50 @@ DROP TABLE Parc;
 DROP TABLE Client;
 
 CREATE TABLE Zone(
-    idZone      varchar(5)  CONSTRAINT cp_Zone PRIMARY KEY,
+    idZone      char(5)  CONSTRAINT cp_Zone PRIMARY KEY,
     nomZone     varchar(20) CONSTRAINT nomZone NOT NULL,
     addresseZone varchar(50) CONSTRAINT addrZone NOT NULL
 );
 
 CREATE TABLE Parc(
-    idParc      varchar(5)  CONSTRAINT cp_Parc PRIMARY KEY,
-    idZone      varchar(5)  CONSTRAINT ce_Zone REFERENCES Zone,
+    idParc      char(5)  CONSTRAINT cp_Parc PRIMARY KEY,
+    idZone      char(5)  CONSTRAINT ce_Zone REFERENCES Zone,
     addresseParc varchar(50) CONSTRAINT addrParc NOT NULL,
     nomParc     varchar(20) CONSTRAINT nomParc NOT NULL,
 );
 
 CREATE TABLE Client(
-    idClient    varchar(10) CONSTRAINT cp_Client PRIMARY KEY,
-    nomClient   varchar(20) 
+    idClient    char(10) CONSTRAINT cp_Client PRIMARY KEY,
+    nomClient   varchar(20) CONSTRAINT nomClient NOT NULL,
+    ageClient numeric(3) CONSTRAINT ageClient NOT NULL,
+    sexeClient char(1) CONSTRAINT sexeClient CHECK(sexeClient IN ('M','F')) NOT NULL,
+    addrDomicile varchar(50) CONSTRAINT addrClient NOT NULL,
+    numTel CHAR (10) CONSTRAINT Tel_Client
+        CHECK (Telephone LIKELIKE ('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')),
+    mailClient VARCHAR(30) CONSTRAINT mailClient NOT NULL
+);
+
+
+
+CREATE TABLE Modele(
+
+    numModel char(5) CONSTRAINT cp_Modele PRIMARY KEY,
+    puissance numeric(3) CONSTRAINT puissance NOT NULL,
+    nbPlaces numeric(2) CONSTRAINT nombrePlaces NOT NULL,
+    nbPortes numeric(2) CONSTRAINT nombrePortes NOT NULL,
+    longueur float (2) CONSTRAINT longueurModele NOT NULL,
+    hauteur float (2) CONSTRAINT hauteurModele NOT NULL
+)
+
+CREATE TABLE Fournisseur(
+
+    numFournisseur char(5) CONSTRAINT numeroFournisseur PRIMARY KEY,
+    addrFournisseur varchar(50) CONSTRAINT addrresseFournisseur NOT NULL,
+    nomFournisseur varchar(20) CONSTRAINT nomFournisseur NOT NULL
+)
+
+CREATE TABLE Type (
+
+    numType char(5) CONSTRAINT cp_Type PRIMARY KEY,
+
 )
