2bf13a529cce2d441327fccb2b4b0d80bff616e3
Alexandre Agostinho <alexandre.agostinho@etu.uca.fr>
2022-12-10 17:51:47 +0100

    requete 3 done

--- sql-scripts/requetes/requete_3.sql
+++ sql-scripts/requetes/requete_3.sql
@@ -4,4 +4,36 @@
     Liste les vehicules qui n'ont jamais ete loue.
 */
 
-SELECT  COUNT(idVehicule
+SELECT  COUNT(idVehicule), idVehicule
+FROM    Vehicule
+EXCEPT
+SELECT  idVehicule
+FROM    Vehicule
+WHERE   nb_loc > 0
+;
+
+
+
+SELECT  COUNT(idVehicule), idVehicule
+FROM    Vehicule
+WHERE   nb_loc <= 0
+;
+
+
+
+SELECT  COUNT(idVehicule), idVehicule
+FROM    Vehicule
+WHERE   nb_loc NOT IN(SELECT idVehicule
+                      FROM   Vehicule
+                      WHERE  nb_loc > 0)
+;
+
+
+
+SELECT  COUNT(idVehicule), idVehicule
+FROM    Vehicule
+MINUS
+SELECT  idVehicule
+FROM    Vehicule
+WHERE   nb_loc > 0
+;
